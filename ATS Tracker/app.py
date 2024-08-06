3c169d99fde670609690c1bc11f8f7a653f4cbf3
Sri Charan Bodduna <68746674+sricharan245@users.noreply.github.com>
2024-08-06 17:20:27 -0500

    updated app by finetuning prompts and added new functionalities

--- ATS Tracker/app.py
+++ ATS Tracker/app.py
@@ -1,7 +1,7 @@
-# Field to put my JD
-# upload PDF
-# PDF to image --> processing --> Google Gemini Pro
-# Prompts Tempalet [ Multi]
+################
+# @author: Sri Charan Bodduna
+# @date: 08/06/2024
+################
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,9 +17,15 @@ import base64
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY")) 
 
-def get_gemini_response(input, pdf_content,  prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input, pdf_content[0], prompt])
+def get_gemini_response(prompt, pdf_content = None,  input = None):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    # print(input)
+    if pdf_content != None and input != None:
+        response = model.generate_content([input, pdf_content[0], prompt])
+    elif pdf_content == None and input == None:
+        response = model.generate_content(prompt)
+    else:
+        return "No Response, please check inputs.."
     return response.text
 
 def input_pdf_setup(uploaded_file):
@@ -43,46 +49,88 @@ def input_pdf_setup(uploaded_file):
         raise FileNotFoundError("file not found")
     
 
-st.set_page_config(page_title = "ATS App")
-st.header("ATS Tracking System")
+st.set_page_config(page_title = "Job Buddy AI")
+st.header("Job Buddy.AI")
+
 input_text = st.text_area("Job Description: ", key="input")
-uploaded_file = st.file_uploader("upload your resume in pdf", type=["pdf"])
+uploaded_file = st.file_uploader("Upload your Resume in PDF format", type=["pdf"])
 
 
-submit1 = st.button("Tell me about resume")
+subPcentMatch = st.button("Percentage Match")
 
-submit2 = st.button("How can I improvise my skills")
+subAboutResume = st.button("Tell me about Resume")
 
-submit3 = st.button("percentage match")
+subImproveSkills = st.button("How can I improvize my Skills")
 
-input_prompt1 = """
-     You are an experienced Technical Human Resource Manager in the field of data science,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+
+subMotivate = st.button("Motivate Me!")
+
+subLkdnReco = st.button("Give LinkedIn Recommendation")
+
+about_resume_prompt = """
+You are an experienced Technical Recruiting Manager in the field of data science and analytics, your task is to review the provided resume against the job description. 
+Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+Highlight the strengths and weaknesses of the applicant in relation to the specified job descriptions.
+"""
+improve_skills_prompt = """
+You are an technical mentor, who helps people to improve their skills in the field of data science, data analytics and data engineering. Your task is to help me to improve skills that I lack in my resume based on job description.
+
+Please provide answer highlighting skills that I lack, then ways to improve them.
 """
 
-input_prompt3 = """
+pcent_match_prompt = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science, data engineering, data analyst and deep ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
 
+motivate_prompt = """
+You are an experienced motivator who knows how to keep people's energy up even when things are difficult. I want you to give me some energising and motivational advice for feeling worried, frustrated and angry about not getting job. I want you to help me look after my mind, my body and my spirit. Give me encouragement, positivity and practical actions I can take. Present your response in markdown.
+"""
 
-if submit1:
+lkdn_reco_prompt = """
+I am currently looking for a new job as data scientist and I want to add more recommendations to my LinkedIn profile to show potential new employers that I am trustworthy and experienced. Please can you help me write a short message to people I have worked with, asking them for a recommendation. Write it as a text message of no more than 100 words.
+"""
+
+
+if subPcentMatch:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt1,pdf_content,input_text)
-        st.subheader("The Repsonse is")
+        response=get_gemini_response(pcent_match_prompt,pdf_content,input_text.lower())
+        st.subheader("The Response is")
         st.write(response)
     else:
         st.write("Please upload the resume")
 
-
-elif submit3:
+elif subAboutResume:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("The Repsonse is")
+        response=get_gemini_response(about_resume_prompt,pdf_content,input_text.lower())
+        st.subheader("The Response is")
         st.write(response)
     else:
         st.write("Please upload the resume")
+
+elif subImproveSkills:
+    # print (input_text)
+    # print()
+    # print(uploaded_file)
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(improve_skills_prompt,pdf_content,input_text.lower())
+        st.subheader("The Response is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+
+elif subMotivate:
+    # pdf_content=input_pdf_setup(uploaded_file)
+    response=get_gemini_response(motivate_prompt)
+    st.subheader("The Response is")
+    st.write(response)
+
+elif subLkdnReco:
+    response=get_gemini_response(lkdn_reco_prompt)
+    st.subheader("The Response is")
+    st.write(response)
+   
